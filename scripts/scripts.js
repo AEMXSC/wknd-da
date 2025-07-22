cffe2576440f463ece98f182fd30398e64bcf63f
Navdeep Singh <navdeeps@adobe.com>
2025-07-22 19:59:02 +0530

    add styling for header,hero, cards, columns

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,35 +1,77 @@
 import {
-  buildBlock,
   loadHeader,
   loadFooter,
-  decorateButtons,
+  decorateButtons as libDecorateButtons,
   decorateIcons,
+  decorateSections,
   decorateBlocks,
   decorateTemplateAndTheme,
-  getMetadata,
   waitForFirstImage,
   loadSection,
   loadSections,
+  loadBlocks,
   loadCSS,
-  sampleRUM,
-  readBlockConfig,
+  fetchPlaceholders,
+  getMetadata,
+  loadScript,
   toClassName,
-  toCamelCase,
+  toCamelCase
 } from './aem.js';
+import { picture, source, img } from './dom-helpers.js';
+
+import {
+  getLanguage,
+  formatDate,
+  setPageLanguage,
+  PATH_PREFIX,
+} from './utils.js';
+
 
 /**
- * Builds hero block and prepends to main in a new section.
- * @param {Element} main The container element
+ * Moves all the attributes from a given elmenet to another given element.
+ * @param {Element} from the element to copy attributes from
+ * @param {Element} to the element to copy attributes to
  */
-function buildHeroBlock(main) {
-  const h1 = main.querySelector('h1');
-  const picture = main.querySelector('picture');
-  // eslint-disable-next-line no-bitwise
-  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
-    const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
-    main.prepend(section);
+export function moveAttributes(from, to, attributes) {
+  if (!attributes) {
+    // eslint-disable-next-line no-param-reassign
+    attributes = [...from.attributes].map(({ nodeName }) => nodeName);
   }
+  attributes.forEach((attr) => {
+    const value = from.getAttribute(attr);
+    if (value) {
+      to.setAttribute(attr, value);
+      from.removeAttribute(attr);
+    }
+  });
+}
+
+export function isAuthorEnvironment() {
+  if(window?.location?.origin?.includes('author')){
+    return true;
+  }else{
+    return false;
+  }
+  /*
+  if(document.querySelector('*[data-aue-resource]') !== null){
+    return true;
+  }*/
+  //return false;
+}
+
+/**
+ * Move instrumentation attributes from a given element to another given element.
+ * @param {Element} from the element to copy attributes from
+ * @param {Element} to the element to copy attributes to
+ */
+export function moveInstrumentation(from, to) {
+  moveAttributes(
+    from,
+    to,
+    [...from.attributes]
+      .map(({ nodeName }) => nodeName)
+      .filter((attr) => attr.startsWith('data-aue-') || attr.startsWith('data-richtext-')),
+  );
 }
 
 /**
@@ -44,24 +86,36 @@ async function loadFonts() {
   }
 }
 
-function autolinkModals(doc) {
-  doc.addEventListener('click', async (e) => {
-    const origin = e.target.closest('a');
-    if (origin && origin.href && origin.href.includes('/modals/')) {
-      e.preventDefault();
-      const { openModal } = await import(`${window.hlx.codeBasePath}/blocks/modal/modal.js`);
-      openModal(origin.href);
+/**
+ * Return the placeholder file specific to language
+ * @returns
+ */
+export async function fetchLanguagePlaceholders() {
+  const langCode = getLanguage();
+  try {
+    // Try fetching placeholders with the specified language
+    return await fetchPlaceholders(`${PATH_PREFIX}/${langCode}`);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(`Error fetching placeholders for lang: ${langCode}. Will try to get en placeholders`, error);
+    // Retry without specifying a language (using the default language)
+    try {
+      return await fetchPlaceholders(`${PATH_PREFIX}/en`);
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error('Error fetching placeholders:', err);
     }
-  });
+  }
+  return {}; // default to empty object
 }
 
 /**
  * Builds all synthetic blocks in a container element.
  * @param {Element} main The container element
  */
-function buildAutoBlocks(main) {
+function buildAutoBlocks() {
   try {
-    if (!main.querySelector('.hero')) buildHeroBlock(main);
+    // TODO: add auto block, if needed
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Auto Blocking failed', error);
@@ -69,55 +123,55 @@ function buildAutoBlocks(main) {
 }
 
 /**
- * Decorates all sections in a container element.
+ * Builds all synthetic blocks in a container element.
  * @param {Element} main The container element
  */
-function decorateSections(main) {
-  main.querySelectorAll(':scope > div').forEach((section) => {
-    const wrappers = [];
-    let defaultContent = false;
-    [...section.children].forEach((e) => {
-      if (e.classList.contains('richtext')) {
-        e.removeAttribute('class');
-        if (!defaultContent) {
-          const wrapper = document.createElement('div');
-          wrapper.classList.add('default-content-wrapper');
-          wrappers.push(wrapper);
-          defaultContent = true;
-        }
-      } else if (e.tagName === 'DIV' || !defaultContent) {
-        const wrapper = document.createElement('div');
-        wrappers.push(wrapper);
-        defaultContent = e.tagName !== 'DIV';
-        if (defaultContent) wrapper.classList.add('default-content-wrapper');
-      }
-      wrappers[wrappers.length - 1].append(e);
-    });
+function decorateButtons(main) {
+  main.querySelectorAll('img').forEach((img) => {
+    let altT = decodeURIComponent(img.alt);
 
-    // Add wrapped content back
-    wrappers.forEach((wrapper) => section.append(wrapper));
-    section.classList.add('section');
-    section.dataset.sectionStatus = 'initialized';
-    section.style.display = 'none';
-
-    // Process section metadata
-    const sectionMeta = section.querySelector('div.section-metadata');
-    if (sectionMeta) {
-      const meta = readBlockConfig(sectionMeta);
-      Object.keys(meta).forEach((key) => {
-        if (key === 'style') {
-          const styles = meta.style
-            .split(',')
-            .filter((style) => style)
-            .map((style) => toClassName(style.trim()));
-          styles.forEach((style) => section.classList.add(style));
+    if (altT && altT.includes('https://delivery-')) {
+      try {
+        altT = JSON.parse(altT);
+        const { altText, deliveryUrl } = altT;
+        const url = new URL(deliveryUrl);
+        const imgName = url.pathname.substring(url.pathname.lastIndexOf('/') + 1);
+        const block = whatBlockIsThis(img);
+        const bp = getMetadata(block);
+        let breakpoints = [{ media: '(min-width: 600px)', width: '2000' }, { width: '750' }];
+        if (bp) {
+          const bps = bp.split('|');
+          const bpS = bps.map((b) => b.split(',').map((p) => p.trim()));
+          breakpoints = bpS.map((n) => {
+            const obj = {};
+            n.forEach((i) => {
+              const t = i.split(/:(.*)/s);
+              obj[t[0].trim()] = t[1].trim();
+            });
+            return obj;
+          });
         } else {
-          section.dataset[toCamelCase(key)] = meta[key];
+          const format = getMetadata(imgName.toLowerCase().replace('.', '-'));
+          const formats = format.split('|');
+          const formatObj = {};
+          formats.forEach((i) => {
+            const [a, b] = i.split('=');
+            formatObj[a] = b;
+          });
+          breakpoints = breakpoints.map((n) => (
+            { ...n, ...formatObj }
+          ));
         }
-      });
-      sectionMeta.parentNode.remove();
+        const picture = createOptimizedPicture(deliveryUrl, altText, false, breakpoints);
+        img.parentElement.replaceWith(picture);
+      } catch (error) {
+        img.setAttribute('style', 'border:5px solid red');
+        img.setAttribute('data-asset-type', 'video');
+        img.setAttribute('title', 'Update block to render video.');
+      }
     }
   });
+  libDecorateButtons(main);
 }
 
 /**
@@ -132,6 +186,29 @@ export function decorateMain(main) {
   buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
+  decorateDMImages(main);
+}
+
+
+async function renderWBDataLayer() {
+  
+  const config = await fetchPlaceholders(PATH_PREFIX);
+  const lastPubDateStr = getMetadata('published-time');
+  const firstPubDateStr = getMetadata('content_date') || lastPubDateStr;
+  window.wbgData.page = {
+    pageInfo: {
+      pageCategory: getMetadata('pagecategory'),
+      channel: getMetadata('channel'),
+      themecfreference: getMetadata('theme_cf_reference'),
+      contentType: getMetadata('content_type'),
+      pageUid: getMetadata('pageuid'),
+      pageName: getMetadata('pagename'),
+      hostName: getMetadata('hostname'),
+      pageFirstPub: formatDate(firstPubDateStr),
+      pageLastMod: formatDate(lastPubDateStr),
+      webpackage: '',
+    },
+  };
 }
 
 /**
@@ -139,19 +216,16 @@ export function decorateMain(main) {
  * @param {Element} doc The container element
  */
 async function loadEager(doc) {
-  doc.documentElement.lang = 'en';
+  //setPageLanguage();
+  document.documentElement.lang = 'en';
   decorateTemplateAndTheme();
-  if (getMetadata('breadcrumbs').toLowerCase() === 'true') {
-    doc.body.dataset.breadcrumbs = true;
-  }
+  renderWBDataLayer();
   const main = doc.querySelector('main');
   if (main) {
     decorateMain(main);
-    doc.body.classList.add('appear');
+    document.body.classList.add('appear');
     await loadSection(main.querySelector('.section'), waitForFirstImage);
   }
-
-  sampleRUM.enhance();
 
   try {
     /* if desktop (proxy for fast connection) or fonts already loaded, load fonts.css */
@@ -168,8 +242,6 @@ async function loadEager(doc) {
  * @param {Element} doc The container element
  */
 async function loadLazy(doc) {
-  autolinkModals(doc);
-
   const main = doc.querySelector('main');
   await loadSections(main);
 
@@ -184,53 +256,117 @@ async function loadLazy(doc) {
   loadFonts();
 }
 
+
+/**
+ * Decorates Dynamic Media images by modifying their URLs to include specific parameters
+ * and creating a <picture> element with different sources for different image formats and sizes.
+ *
+ * @param {HTMLElement} main - The main container element that includes the links to be processed.
+ */
+export function decorateDMImages(main) {
+  main.querySelectorAll('a[href^="https://delivery-p"]').forEach((a) => {
+    const url = new URL(a.href.split('?')[0]);
+    if (url.hostname.endsWith('.adobeaemcloud.com')) {
+
+        const blockBeingDecorated = whatBlockIsThis(a);
+        let blockName = '';
+        let rotate = '';
+        let flip = '';
+        let crop = '';
+        if(blockBeingDecorated){
+            blockName = Array.from(blockBeingDecorated.classList).find(className => className !== 'block');
+        }
+        if(blockName && blockName === 'dynamicmedia-image'){
+          rotate = blockBeingDecorated?.children[3]?.textContent?.trim();
+          flip = blockBeingDecorated?.children[4]?.textContent?.trim();
+          crop = blockBeingDecorated?.children[5]?.textContent?.trim();
+        }
+
+        const uuidPattern = /([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/i;
+        const match = url.href?.match(uuidPattern);
+        let aliasname = '';
+        if (!match) {
+            throw new Error('No asset UUID found in URL');
+        }else{
+          aliasname = match[1];
+        }
+        let hrefWOExtn =  url.href?.substring(0, url.href?.lastIndexOf('.'))?.replace(/\/original\/(?=as\/)/, '/');
+        const pictureEl = picture(
+          source({ 
+              srcset: `${hrefWOExtn}.webp?width=1400&quality=85&preferwebp=true${rotate ? '&rotate=' + rotate : ''}${flip ? '&flip=' + flip.toLowerCase() : ''}${crop ? '&crop=' + crop.toLowerCase() : ''}`, 
+              type: 'image/webp', 
+              media: '(min-width: 992px)' 
+          }),
+          source({ 
+              srcset: `${hrefWOExtn}.webp?width=1320&quality=85&preferwebp=true${rotate ? '&rotate=' + rotate : ''}${flip ? '&flip=' + flip.toLowerCase() : ''}${crop ? '&crop=' + crop.toLowerCase() : ''}`, 
+              type: 'image/webp', 
+              media: '(min-width: 768px)' 
+          }),
+          source({ 
+              srcset: `${hrefWOExtn}.webp?width=780&quality=85&preferwebp=true${rotate ? '&rotate=' + rotate : ''}${flip ? '&flip=' + flip.toLowerCase() : ''}${crop ? '&crop=' + crop.toLowerCase() : ''}`, 
+              type: 'image/webp', 
+              media: '(min-width: 320px)' 
+          }),
+          source({ 
+              srcset: `${hrefWOExtn}.webp?width=1400&quality=85${rotate ? '&rotate=' + rotate : ''}${flip ? '&flip=' + flip.toLowerCase() : ''}${crop ? '&crop=' + crop.toLowerCase() : ''}`, 
+              media: '(min-width: 992px)' 
+          }),
+          source({ 
+              srcset: `${hrefWOExtn}.webp?width=1320&quality=85${rotate ? '&rotate=' + rotate : ''}${flip ? '&flip=' + flip.toLowerCase() : ''}${crop ? '&crop=' + crop.toLowerCase() : ''}`, 
+              media: '(min-width: 768px)' 
+          }),
+          source({ 
+              srcset: `${hrefWOExtn}.webp?width=780&quality=85${rotate ? '&rotate=' + rotate : ''}${flip ? '&flip=' + flip.toLowerCase() : ''}${crop ? '&crop=' + crop.toLowerCase() : ''}`, 
+              media: '(min-width: 320px)' 
+          }),
+          img({ 
+              src: `${hrefWOExtn}.webp?width=1400&quality=85${rotate ? '&rotate=' + rotate : ''}${flip ? '&flip=' + flip.toLowerCase() : ''}${crop ? '&crop=' + crop.toLowerCase() : ''}`, 
+              alt: a.innerText 
+          }),
+        );
+      a.replaceWith(pictureEl);
+    }
+  });
+}
+
+function whatBlockIsThis(element) {
+  let currentElement = element;
+
+  while (currentElement.parentElement) {
+    if (currentElement.parentElement.classList.contains('block')) return currentElement.parentElement;
+    currentElement = currentElement.parentElement;
+    if (currentElement.classList.length > 0) return currentElement.classList[0];
+  }
+  return null;
+}
+
+/**
+ * remove the adujusts the auto images
+ * @param {Element} main The container element
+ */
+function adjustAutoImages(main) {
+  const pictureElement = main.querySelector('div > p > picture');
+  if (pictureElement) {
+    const pElement = pictureElement.parentElement;
+    pElement.className = 'auto-image-container';
+  }
+}
+
 /**
  * Loads everything that happens a lot later,
  * without impacting the user experience.
  */
 function loadDelayed() {
+  // eslint-disable-next-line import/no-cycle
   window.setTimeout(() => import('./delayed.js'), 3000);
   // load anything that can be postponed to the latest here
 }
 
-async function loadSidekick() {
-  if (document.querySelector('aem-sidekick')) {
-    import('./sidekick.js');
-    return;
-  }
-
-  document.addEventListener('sidekick-ready', () => {
-    import('./sidekick.js');
-  });
-}
-
 async function loadPage() {
+  window.wbgData ||= {};
   await loadEager(document);
   await loadLazy(document);
   loadDelayed();
-  loadSidekick();
-}
-
-// UE Editor support before page load
-if (window.location.hostname.includes('ue.da.live')) {
-  // eslint-disable-next-line import/no-unresolved
-  await import(`${window.hlx.codeBasePath}/ue/scripts/ue.js`).then(({ default: ue }) => ue());
 }
 
 loadPage();
-
-const { searchParams, origin } = new URL(window.location.href);
-const branch = searchParams.get('nx') || 'main';
-
-export const NX_ORIGIN = branch === 'local' || origin.includes('localhost') ? 'http://localhost:6456/nx' : 'https://da.live/nx';
-
-(async function loadDa() {
-  /* eslint-disable import/no-unresolved */
-  if (searchParams.get('dapreview')) {
-    import('https://da.live/scripts/dapreview.js')
-      .then(({ default: daPreview }) => daPreview(loadPage));
-  }
-  if (searchParams.get('daexperiment')) {
-    import(`${NX_ORIGIN}/public/plugins/exp/exp.js`);
-  }
-}());
